0dc072e4778e1d44bffbe8f83f734e0073877d0f
woffles <wolfgang.truong@gmail.com>
2018-10-05 22:09:01 -0700

    update to server file

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ if(process.env.NODE_ENV === 'production') {
     // Set static folder to client/build
     app.use(express.static('client/build'));
     // any route gets hit here load the react html file in build
-    app.get('*', (req,res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 
